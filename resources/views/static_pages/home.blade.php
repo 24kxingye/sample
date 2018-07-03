12f65691e296d7aa7b3b27a88540689e8410e477
24kxingye <qingyuxing420@gmail.com>
2018-07-03 07:45:50 +0000

    完成注册页面与路由

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>首页</h1>
+    <div class="jumbotron">
+        <h1>Laravel APP</h1>
+    <p class="lead">
+        你现在看到的是24k星爷Laravel练习
+    </p>
+    <p>
+        一切，从这里开始
+    </p>
+    <p>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    </p>
+    </div>
 @stop
